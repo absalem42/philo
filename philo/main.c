41ca9bd6a71d84c70fa61b4cf604fc01adeabb59
Abdullah <a@A.>
2024-01-06 21:47:46 +0400

    d

--- philo/main.c
+++ philo/main.c
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: absalem < absalem@student.42abudhabi.ae    +#+  +:+       +#+        */
+/*   By: absalem <absalem@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 15:28:18 by absalem           #+#    #+#             */
-/*   Updated: 2024/01/04 15:13:16 by absalem          ###   ########.fr       */
+/*   Updated: 2024/01/06 21:47:24 by absalem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-// void	philo_init(t_philo **philo_p, char **av)
-// {
-// 	if (!av)
-// 		return ;
-// 	if (check_num(av))
-// 	{
-// 		ft_putstr_fd("invliad irgument",2);
-// 	}
-// }
+void	philo_init(t_philo **philo_p, char **av)
+{
+	if (!av)
+		return ;
+	if (check_num(av))
+	{
+		ft_putstr_fd("invliad irgument",2);
+	}
+}
 
 int main(int ac, char **av)
 {
